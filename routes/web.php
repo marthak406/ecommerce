df4bf272f909f7428c872790ba7ce8f736495fc2
marthak406 <marthakristiana406@gmail.com>
2024-07-31 20:49:39 +0700

    routes for admin and customer

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Middleware\CheckRole;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [ProductController::class, 'home'])->name('home');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('show');
+
+// untuk admin
+Route::prefix('admin')->group(function () {
+    Route::get('/', [LoginController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('/login', [LoginController::class, 'login'])->name('admin.login.submit');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('admin.logout');
+
+    Route::middleware(['auth:admin',CheckRole::class.':admin'])->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+        Route::resource('/products', ProductController::class);
+    });
 });
