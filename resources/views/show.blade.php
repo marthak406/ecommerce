a08469f73509350affd75a342b6c164cecb09101
marthak406 <marthakristiana406@gmail.com>
2024-08-05 16:20:00 +0700

    modify number format

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Show Products</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="{{ route('home') }}">E-Commerce</a>
-        </nav>
-    </header>
+@extends('layouts.home')
 
+@section('title', 'Detail Product')
+
+@section('content')
     <div class="container mt-5 mb-5">
         <div class="row">
             <div class="col-md-4">
@@ -39,7 +28,4 @@
             </div>
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
+@endsection
